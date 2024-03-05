36b3ed91dcc4610f0d4deafe86301b7dfffca64a
Danny Holt <danielholt505@gmail.com>
2024-03-05 15:47:51 -0600

    Played around with enemy movement, and added very basic health system. I realize that later on, the enemy attributes should come from a class.

--- MonsterDefense/Assets/Scripts/Enemy1.cs
+++ MonsterDefense/Assets/Scripts/Enemy1.cs
@@ -6,17 +6,22 @@ public class Enemy1 : MonoBehaviour
 {
     const int movingSpeed = 1;
     Rigidbody2D rb;
+    int health;
+
     // Start is called before the first frame update
-    
     void Start()
     {
         rb = GetComponent <Rigidbody2D>();
         rb.velocity = new Vector2(movingSpeed, 0);
+        health = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        health --;
+        if(health <= 0){
+            Destroy(gameObject);
+        }
     }
 }
